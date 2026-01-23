419aac6d10802d8582d6dbb3ef8b13cafd41bf8a
looney-blooper <mithunarulmani017@gmail.com>
2026-01-23 19:30:57 +0530

    minor changes

--- train.py
+++ train.py
@@ -15,8 +15,8 @@ FILES = ["los-angeles-1"]        # list of mat filenames (without .mat)
 SEED = 42
 
 # training hyperparams
-BATCH_SIZE = 32
-END_ITER = 75
+BATCH_SIZE = 64
+END_ITER = 150
 SEARCH_ITER = 25
 LR = 1e-3
 
